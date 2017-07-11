460aca577a96a12cd5bd1a604298cac113d7b52b
Korneliu <olbiton@gmail.com>
2017-07-10 22:18:17 -0700

    added displayNYTdata function and changed intro divs

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 //banner
-$('.intro').text('whatever').delay(2000).fadeOut(4000);
+$('.intro1').fadeOut(5000);
+$('.intro2').hide().fadeIn(7000).show(4000).fadeOut(3000);
 
 
 function getDataFromAPI(searchTerm, callback) {
-	const = NYT_SEARCH_URL = 'http://api.nytimes.com/svc/search/v1';
+	const  NYT_SEARCH_URL = 'http://api.nytimes.com/svc/search/v1';
 	const query = {
-
-	}
+		part: 'snippet',
+		q: searchTerm
+    }
 }
 
 
@@ -15,7 +17,9 @@ function getDataFromAPI(searchTerm, callback) {
 
 
 function displayNewYorkTimesData(data) {
-
+	console.log(data);
+	const results = data.items.map((item, index) => renderResult(item));
+  $('#results').html(results);
 };
 
 function watchSubmit() {
