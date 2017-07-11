3dcd18f3907d88767d2f35c26b1dfe9250095df6
Korneliu <olbiton@gmail.com>
2017-07-10 23:16:13 -0700

    added renderResults function and getDataFromAPI

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 //banner
-$('.intro1').fadeOut(5000);
-$('.intro2').hide().fadeIn(7000).show(4000).fadeOut(3000);
-
+//$('.intro1').fadeOut(5000);
+//$('.intro2').hide().fadeIn(7000).show(4000).fadeOut(3000);
 
 function getDataFromAPI(searchTerm, callback) {
-	const  NYT_SEARCH_URL = 'http://api.nytimes.com/svc/search/v1';
-	const query = {
-		part: 'snippet',
-		q: searchTerm
-    }
+	const  NYT_SEARCH_URL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+	const query = {						
+		part: 'headline',
+		key : e7003c070aea44feb70e1298fd660497,
+		q: searchTerm,
+		per_page: 5
+  }
+  $.getJSON(NYT_SEARCH_URL, query, callback);
+};
+
+
+function renderResults (item) {
+	return `
+		<div>
+			<h3>${docs.headline.main}</h3>
+		</div>`
 }
-
-
-
-// render results
 
 
 function displayNewYorkTimesData(data) {
@@ -28,8 +34,9 @@ $('#searchForm').submit(event => {
 	const searchTerm = $('#searchInput').val();
 	$('#searchInput').val("");
 	getDataFromAPI(searchTerm, displayNewYorkTimesData);
-
 });
 };
+
+$(watchSubmit);
 
 
