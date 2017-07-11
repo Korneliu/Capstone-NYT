f41f53051b9f698778293426ace2ff0f1de443d1
Korneliu <olbiton@gmail.com>
2017-07-10 21:28:14 -0700

    added watchSubmit function and form section

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 //banner
-$('.intro').text('whatever');
+$('.intro').text('whatever').delay(2000).fadeOut(4000);
 
-//get data from API
+
+function getDataFromAPI(searchTerm, callback) {
+	const = NYT_SEARCH_URL = 'http://api.nytimes.com/svc/search/v1';
+	const query = {
+
+	}
+}
 
 
 
 // render results
 
 
-//displayNYT data
+function displayNewYorkTimesData(data) {
 
+};
 
+function watchSubmit() {
+$('#searchForm').submit(event => {
+	event.preventDefault();
+	const searchTerm = $('#searchInput').val();
+	$('#searchInput').val("");
+	getDataFromAPI(searchTerm, displayNewYorkTimesData);
 
-//watch submit function
+});
+};
 
 
