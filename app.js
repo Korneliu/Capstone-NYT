528f75dcb0da4200b6af60426357775a3745be07
Korneliu <olbiton@gmail.com>
2017-07-13 23:10:54 -0700

    changed how excluded wordsa are display

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 //banner
-//$('.intro1').fadeOut(5000);
-//$('.intro2').hide().fadeIn(7000).show(4000).fadeOut(3000);
+$('.intro1').fadeOut(5000);
+$('.intro2').hide().fadeIn(3000).show(2000).fadeOut(2000);
+$('#searchForm').fadeIn(3000);
+
+let excluded = [];
+
+$()
 
 function getDataFromAPI(searchTerm, callback) {
 	const  NYT_SEARCH_URL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
 	const query = {						
-		part: 'headline',
-		key : e7003c070aea44feb70e1298fd660497,
+		part: 'response',
+		'api-key': 'e7003c070aea44feb70e1298fd660497',
 		q: searchTerm,
-		per_page: 5
+		per_page: 10
   }
   $.getJSON(NYT_SEARCH_URL, query, callback);
 };
@@ -17,24 +22,37 @@ function getDataFromAPI(searchTerm, callback) {
 function renderResults (item) {
 	return `
 		<div>
-			<h3>${docs.headline.main}</h3>
+			<a href="${item.web_url}" target="_blank"><h3>${item.headline.main}</h3></a>
 		</div>`
-}
-
+};
 
 function displayNewYorkTimesData(data) {
 	console.log(data);
-	const results = data.items.map((item, index) => renderResult(item));
+	const results = data.response.docs.map((item, index) => renderResults(item));
   $('#results').html(results);
 };
 
 function watchSubmit() {
-$('#searchForm').submit(event => {
-	event.preventDefault();
-	const searchTerm = $('#searchInput').val();
-	$('#searchInput').val("");
-	getDataFromAPI(searchTerm, displayNewYorkTimesData);
-});
+	$('#searchForm').submit(event => {
+		event.preventDefault();
+		const searchTerm = $('#searchInput').val();
+		$('#searchInput').val("");
+		getDataFromAPI(searchTerm, displayNewYorkTimesData);
+	});
+
+	$('#excludedForm').submit(event => {
+		event.preventDefault();
+		let word = $('#excludedInput').val();
+		$('#excludedInput').val("");
+		excluded.push(word);
+		$('#excluded').append(`<button class="square">${word}</button`);
+		$('.square').on('click', function(event) {
+			$(this).hide();
+		});
+	});
+
+	//change headline to array with strings
+	//compare array with word
 };
 
 $(watchSubmit);
